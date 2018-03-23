dfcde13152f0448c93e3b412fc4eea1e97ba43cd
Giovanni Olivera <giovanni@commit42.fr>
2018-03-23 14:41:43 +0100

    Some content

--- server.js
+++ server.js
@@ -4,95 +4,103 @@ const app = express();
 var Redmine = require("node-redmine");
 var RSS = require("rss");
 
-// protocol required in Hostname, supports both HTTP and HTTPS
-var hostname = "";
-var config = {
-  apiKey: ""
-};
+var cleanUrl = url =>
+  ((/^https?:\/\//i.test(url) && url) || `https://${url}`).replace(/\/$/, "");
 
-function getRSS() {
-  var redmine = new Redmine(hostname, config);
+function getRSS(url, apiKey) {
+  const cleaned_url = cleanUrl(url || process.env.URL);
+
+  var redmine = new Redmine(cleaned_url, {
+    apiKey: apiKey || process.env.API_KEY
+  });
 
   /**
    * Dump issue
    */
-  var dump_issue = function(issue, feed) {
-    feed.item(issue);
+  var add_issue = function(issue, feed) {
+    console.log(issue);
+    console.log(issue.journals);
+    feed.item({
+      title: `[${issue.project.name}] #${issue.id} - ${issue.subject} [${
+        issue.tracker.name
+      }]`,
+      categories: [issue.tracker.name],
+      url: `${cleaned_url}/issues/${issue.id}${
+        issue.journals.length
+          ? "#change-" + issue.journals[issue.journals.length - 1].id
+          : ""
+      }`,
+      date: issue.updated_on.toUTCString(),
+      author: (issue.journals.length
+        ? issue.journals[issue.journals.length - 1].user
+        : issue.author
+      ).name
+    });
   };
 
   return new Promise((resolve, reject) => {
-    redmine.issues({ limit: 100 }, function(err, data) {
-      if (err) reject(err);
+    redmine.issues({ limit: 30 }, function(err, data) {
+      if (err) return reject(err);
 
-      /* lets create an rss feed */
-      var feed = new RSS({
-        title: "title",
-        description: "description",
-        feed_url: "http://example.com/rss.xml",
-        site_url: "http://example.com",
-        image_url: "http://example.com/icon.png",
-        docs: "http://example.com/rss/docs.html",
-        managingEditor: "Dylan Greene",
-        webMaster: "Dylan Greene",
-        copyright: "2013 Dylan Greene",
-        language: "en",
-        categories: ["Category 1", "Category 2", "Category 3"],
-        pubDate: "May 20, 2012 04:00:00 GMT",
-        ttl: "60",
-        custom_namespaces: {
-          itunes: "http://www.itunes.com/dtds/podcast-1.0.dtd"
-        },
-        custom_elements: [
-          { "itunes:subtitle": "A show about everything" },
-          { "itunes:author": "John Doe" },
-          {
-            "itunes:summary":
-              "All About Everything is a show about everything. Each week we dive into any subject known to man and talk about it as much as we can. Look for our podcast in the Podcasts app or in the iTunes Store"
-          },
-          {
-            "itunes:owner": [
-              { "itunes:name": "John Doe" },
-              { "itunes:email": "john.doe@example.com" }
-            ]
-          },
-          {
-            "itunes:image": {
-              _attr: {
-                href:
-                  "http://example.com/podcasts/everything/AllAboutEverything.jpg"
-              }
+      const issues = data.issues.map(issue => {
+        return new Promise((resolve, reject) => {
+          redmine.get_issue_by_id(
+            issue.id,
+            { include: "journals" },
+            (err, data) => {
+              (err && reject(err)) ||
+                resolve(
+                  Object.assign(
+                    {},
+                    issue,
+                    Object.assign({}, data.issue, {
+                      journals: (Array.isArray(data.issue.journals)
+                        ? data.issue.journals
+                        : []
+                      ).sort((a, b) => a.id - b.id)
+                    })
+                  )
+                );
             }
-          },
-          {
-            "itunes:category": [
-              {
-                _attr: {
-                  text: "Technology"
-                }
-              },
-              {
-                "itunes:category": {
-                  _attr: {
-                    text: "Gadgets"
-                  }
-                }
-              }
-            ]
-          }
-        ]
+          );
+        });
       });
 
-      for (var i in data.issues) {
-        dump_issue(data.issues[i], feed);
-      }
+      Promise.all(issues).then(issues => {
+        const final_issues = issues
+          .map(issue => {
+            return Object.assign({}, issue, {
+              updated_on: new Date(Date.parse(issue.updated_on)),
+              created_on: new Date(Date.parse(issue.updated_on))
+            });
+          })
+          .sort((a, b) => b - a);
 
-      resolve(feed.xml({ indent: true }));
+        /* lets create an rss feed */
+        const feed = new RSS({
+          title: "Redmine issues",
+          description: `Redmine issues for ${cleaned_url}`,
+          site_url: cleaned_url,
+          pubDate: (
+            (final_issues && final_issues[0].updated_on) ||
+            new Date()
+          ).toUTCString(),
+          ttl: "1"
+        });
+
+        final_issues.reverse().forEach(i => add_issue(i, feed));
+
+        resolve(feed.xml({ indent: true }));
+      }, reject);
     });
   });
 }
 
-app.get("/", function(req, res) {
-  getRSS().then(res.send.bind(res));
+app.get("*", function(req, res) {
+  getRSS(req.query.url, req.query.api_key).then(rss => {
+    res.header("Content-Type", "application/rss+xml; charset=utf-8");
+    res.send(rss);
+  }, res.send.bind(res));
 });
 
 app.listen(3000, function() {
